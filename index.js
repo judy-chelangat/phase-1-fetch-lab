892374e58c4670373f54be545ecc326beb41b5b8
judy-chelangat <judysigilai75@gmail.com>
2023-06-28 23:26:33 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
+  fetch(`https://anapioficeandfire.com/api/books`)
   .then(resp => resp.json())
-  .then(data => renderBooks(data))
+  .then(data => {
+    console.log(data)
+    renderBooks(data)})
 
 
   function renderBooks(books){
       const main = document.querySelector('main');
       books.forEach(book => {
         const heading = document.createElement('h2');
-        h2.textContent = book.name;
+        heading.textContent = book.name;
         main.appendChild(heading);
       })
   
@@ -17,10 +19,22 @@ function fetchBooks() {
  
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener('DOMContentLoaded', fetchBooks);
+// function getData(){
+//   console.log("2. Returning instantly available data.");
+//   return [{author: "Ta-Nehisi Coates"}, {author: "Cathy Park Hong"}];
+// }
 
-});
+// function main(){
+//   console.log("1. Starting Script");
+//   const data = getData();
+//   console.log(`3. Data is currently ${JSON.stringify(data)}`);
+//   console.log("4. Script Ended");
+// }
+
+// main();
+// setTimeout(() => console.log('Hello World!'), 10000); // 0 Milliseconds!!
+// console.log("No, me first");
 
 
 
